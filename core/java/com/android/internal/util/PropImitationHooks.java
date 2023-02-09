639ebade4ba571f4dbeeeb72a1e5ca2a38f45430
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-02-09 10:51:56 +0530

    PropImitationHooks: Spoof gapps as Pixel
    Change-Id: I2e66cba7c9098dd0063123db103407b2955d1c80

--- core/java/com/android/internal/util/PropImitationHooks.java
+++ core/java/com/android/internal/util/PropImitationHooks.java
@@ -27,6 +27,8 @@ import com.android.internal.R;
 
 import java.lang.reflect.Field;
 import java.util.Arrays;
+import java.util.Map;
+import java.util.Set;
 
 public class PropImitationHooks {
 
@@ -39,16 +41,29 @@ public class PropImitationHooks {
     private static final String sStockFp =
             Resources.getSystem().getString(R.string.config_stockFingerprint);
 
-    // Use stock fingerprint for ARCore to load correct profile
+    private static final boolean sSpoofGapps =
+            Resources.getSystem().getBoolean(R.bool.config_spoofGoogleApps);
+
     private static final String PACKAGE_ARCORE = "com.google.ar.core";
+    private static final String PACKAGE_ASI = "com.google.android.as";
 
     // Use certified properties for GMS to pass SafetyNet / Play Integrity
     private static final String PACKAGE_FINSKY = "com.android.vending";
     private static final String PACKAGE_GMS = "com.google.android.gms";
+    private static final String PACKAGE_VELVET = "com.google.android.googlequicksearchbox";
+    private static final String PACKAGE_SNAPCHAT = "com.snapchat.android";
+
+    private static final String PROCESS_GMS_PERSISTENT = PACKAGE_GMS + ".persistent";
     private static final String PROCESS_GMS_UNSTABLE = PACKAGE_GMS + ".unstable";
 
-    // Use certified properties for Snapchat to prevent send/receive delays
-    private static final String PACKAGE_SNAPCHAT = "com.snapchat.android";
+    private static final Map<String, Object> sPixelProps = Map.of(
+        "BRAND", "google",
+        "MANUFACTURER", "Google",
+        "DEVICE", "redfin",
+        "PRODUCT", "redfin",
+        "MODEL", "Pixel 5",
+        "FINGERPRINT", "google/redfin/redfin:13/TQ1A.230105.001/9292298:user/release-keys"
+    );
 
     private static volatile boolean sIsGms = false;
     private static volatile boolean sIsFinsky = false;
@@ -65,10 +80,12 @@ public class PropImitationHooks {
         sIsGms = packageName.equals(PACKAGE_GMS) && processName.equals(PROCESS_GMS_UNSTABLE);
         sIsFinsky = packageName.equals(PACKAGE_FINSKY);
 
-        /* Set certified properties for GMSCore / Snapchat
+        /* 
+         * Set certified properties for GMSCore
          * Set stock fingerprint for ARCore
+         * Set Pixel 5 for Snapchat, Google, ASI and GMS device configurator
          */
-        if (sCertifiedProps.length == 4 && (sIsGms || packageName.equals(PACKAGE_SNAPCHAT))) {
+        if (sCertifiedProps.length == 4 && sIsGms) {
             dlog("Spoofing build for: " + packageName);
             setPropValue("DEVICE", sCertifiedProps[0]);
             setPropValue("PRODUCT", sCertifiedProps[1]);
@@ -77,10 +94,17 @@ public class PropImitationHooks {
         } else if (!sStockFp.isEmpty() && packageName.equals(PACKAGE_ARCORE)) {
             dlog("Setting stock fingerprint for: " + packageName);
             setPropValue("FINGERPRINT", sStockFp);
+        } else if (sSpoofGapps && (packageName.equals(PACKAGE_VELVET)
+                || packageName.equals(PACKAGE_ASI)
+                || packageName.equals(PACKAGE_SNAPCHAT)
+                || (packageName.equals(PACKAGE_GMS)
+                    && processName.equals(PROCESS_GMS_PERSISTENT)))) {
+            dlog("Spoofing Pixel 5 for: " + packageName + " process: " + processName);
+            sPixelProps.forEach(PropImitationHooks::setPropValue);
         }
     }
 
-    private static void setPropValue(String key, Object value){
+    private static void setPropValue(String key, Object value) {
         try {
             dlog("Setting prop " + key + " to " + value.toString());
             Field field = Build.class.getDeclaredField(key);
@@ -105,7 +129,7 @@ public class PropImitationHooks {
         }
     }
 
-    public static void dlog(String msg) {
+    private static void dlog(String msg) {
       if (DEBUG) Log.d(TAG, msg);
     }
 }
